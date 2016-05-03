6d4af0adaaedcec96d900ad991c8536bb06f29c9
Peggy Lindner <peggylind@gmail.com>
2016-05-02 23:26:55 -0500

    removed "Create Aggregates for selected day" button from all but "admin" user roles

--- client/views/site/site.js
+++ client/views/site/site.js
@@ -10,7 +10,6 @@ Meteor.subscribe('sites');
 Highcharts.setOptions({
   global: {
     useUTC: false,
-
   },
   colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
 });
