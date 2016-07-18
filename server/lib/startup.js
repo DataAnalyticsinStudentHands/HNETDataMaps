960c7cfee368989fc310d22095af3c56ab5fac8a
Lindner, Peggy <plindner@uh.edu>
2016-07-18 14:05:35 -0500

    - added startup sites collection if server has no sites

--- server/lib/startup.js
+++ server/lib/startup.js
@@ -2,3 +2,12 @@
 logger = Winston;
 
 logger.info(`Winston logs are being captured on the console for host: ${require('os').hostname()}.`);
+
+Meteor.startup(function () {
+  // Insert sample data if the site collection is empty
+  if (Sites.find().count() === 0) {
+    JSON.parse(Assets.getText("sites.json")).site.forEach(function (doc) {
+      Sites.insert(doc);
+    });
+  }
+});
