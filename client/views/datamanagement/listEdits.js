c820c8b73080c9462b3c4bad0d6efdecfef65b99
Lindner, Peggy <plindner@uh.edu>
2016-09-14 12:23:20 -0500

    - removed faulty console logs for listEdits page

--- client/views/datamanagement/listEdits.js
+++ client/views/datamanagement/listEdits.js
@@ -1,5 +1,19 @@
 Template.viewEditsCell.events({
-  'click .js-view-edits': function () {
+  'click .js-view-edits': function (event) {
+		var dataTable = $(event.target).closest('table').DataTable();
+		var rowData = dataTable.row(event.currentTarget).data();
+    // Your click handler logic here
     Router.go(`/site/${this.site}`);
-  }
+  },
+});
+
+	Template.listEdits.events({
+
+		'click tr': function (event) {
+	    var dataTable = $(event.target).closest('table').DataTable();
+	    var rowData = dataTable.row(event.currentTarget).data();
+	    if (!rowData) return; // Won't be data if a placeholder row is clicked
+	    // Your click handler logic here
+	  }
+
 });
