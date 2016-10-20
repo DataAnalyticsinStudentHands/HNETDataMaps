3ebe1310216799478ee041f3024b0cfe56cda426
Peggy Lindner <peggylind@gmail.com>
2016-10-19 22:01:38 -0500

    - new version for deployment on server

--- server/cronJobs.js
+++ server/cronJobs.js
@@ -27,51 +27,51 @@ function sendEmail(reportType, reportString) {
 }
 
 // every 10 mins push data
-Meteor.setInterval(() => {
-  // get sites
-  const activeSites = LiveSites.find({ status: 'Active' });
-
-  activeSites.forEach(function(site) {
-    // get closest 5 min intervall
-    const ROUNDING = 5 * 60 * 1000;/* ms */
-    let end = moment();
-    end = moment(Math.floor((+ end) / ROUNDING) * ROUNDING);
-
-    // check last push not older than 24 hours
-    if (site.lastPushEpoch > moment().subtract(1, 'days').unix()) {
-      const startEpoch = site.lastPushEpoch;
-      const endEpoch = moment(end).unix();
-      const startTime = moment.unix(startEpoch).format('YYYY-MM-DD-hh-mm-ss');
-      const endTime = moment.unix(endEpoch).format('YYYY-MM-DD-hh-mm-ss');
-      console.log(`called export from cron for AQSID: ${site.AQSID}, startEpoch: ${startEpoch}, endEpoch: ${endEpoch}, startTime: ${startTime}, endEpoch: ${endTime}`);
-      // create TCEQ export formated data and push
-      Meteor.call('exportData', site.AQSID, startEpoch, endEpoch, (err, data) => {
-        // create csv file to be pushed in temp folder
-        const outputFile = `/hnet/test/${moment.utc().format('YYMMDDHHmmss')}.uh`;
-        const csvComplete = Papa.unparse(data);
-        // removing header from csv string
-        const n = csvComplete.indexOf('\n');
-        const csv = csvComplete.substring(n + 1);
-
-        fs.writeFile(outputFile, csv, function(err) {
-          if (err) {
-            logger.error(`Could not write TCEQ push file. Error: ${err}`);
-            throw new Meteor.Error(`Could not write TCEQ push file. Error: ${err}`);
-          }
-        });
-        if (!err) {
-          LiveSites.update({
-            _id: site._id
-          }, {
-            $set: {
-              lastPushEpoch: endEpoch
-            }
-          }, { validate: false });
-        }
-      });
-    }
-  });
-}, 10 * 60 * 1000); // run every 10 min, to push new data
+// Meteor.setInterval(() => {
+//   // get sites
+//   const activeSites = LiveSites.find({ status: 'Active' });
+//
+//   activeSites.forEach(function(site) {
+//     // get closest 5 min intervall
+//     const ROUNDING = 5 * 60 * 1000;/* ms */
+//     let end = moment();
+//     end = moment(Math.floor((+ end) / ROUNDING) * ROUNDING);
+//
+//     // check last push not older than 24 hours
+//     if (site.lastPushEpoch > moment().subtract(1, 'days').unix()) {
+//       const startEpoch = site.lastPushEpoch;
+//       const endEpoch = moment(end).unix();
+//       const startTime = moment.unix(startEpoch).format('YYYY-MM-DD-hh-mm-ss');
+//       const endTime = moment.unix(endEpoch).format('YYYY-MM-DD-hh-mm-ss');
+//       console.log(`called export from cron for AQSID: ${site.AQSID}, startEpoch: ${startEpoch}, endEpoch: ${endEpoch}, startTime: ${startTime}, endEpoch: ${endTime}`);
+//       // create TCEQ export formated data and push
+//       Meteor.call('exportData', site.AQSID, startEpoch, endEpoch, (err, data) => {
+//         // create csv file to be pushed in temp folder
+//         const outputFile = `/hnet/test/${moment.utc().format('YYMMDDHHmmss')}.uh`;
+//         const csvComplete = Papa.unparse(data);
+//         // removing header from csv string
+//         const n = csvComplete.indexOf('\n');
+//         const csv = csvComplete.substring(n + 1);
+//
+//         fs.writeFile(outputFile, csv, function(err) {
+//           if (err) {
+//             logger.error(`Could not write TCEQ push file. Error: ${err}`);
+//             throw new Meteor.Error(`Could not write TCEQ push file. Error: ${err}`);
+//           }
+//         });
+//         if (!err) {
+//           LiveSites.update({
+//             _id: site._id
+//           }, {
+//             $set: {
+//               lastPushEpoch: endEpoch
+//             }
+//           }, { validate: false });
+//         }
+//       });
+//     }
+//   });
+// }, 10 * 60 * 1000); // run every 10 min, to push new data
 
 // daily reset of values for reports
 Meteor.setInterval(() => {
