6d4f471ae576595d8dec7b847660d84fa989213f
Lindner, Peggy <plindner@uh.edu>
2016-10-25 09:53:19 -0500

    - playing with the idea of individual charts for each measurement to allow for better control

--- client/views/site/site.js
+++ client/views/site/site.js
@@ -1,5 +1,4 @@
 import Highcharts from 'highcharts/highstock';
-require('highcharts/modules/exporting')(Highcharts);
 
 // 3 days
 const startEpoch = new ReactiveVar(moment().subtract(4320, 'minutes').unix());
@@ -133,28 +132,10 @@ function createChart(chartName, titleText, seriesOptions, yAxisOptions) {
         click: unselectByClick
       },
       zoomType: 'xy',
-      renderTo: chartName
-    },
-		exporting: {
-			buttons: {
-                contextButton: {
-                    menuItems: [{
-                        text: 'Export to PNG (small)',
-                        onclick: function () {
-                            this.exportChart({
-                                width: 250
-                            });
-                        }
-                    }, {
-                        text: 'Export to PNG (large)',
-                        onclick: function () {
-                            this.exportChart();
-                        },
-                        separator: false
-                    }]
-                }
-							},
-        enabled: true
+      renderTo: chartName,
+			marginLeft: 100, // Keep all charts left aligned
+                        spacingTop: 20,
+                        spacingBottom: 20
     },
     title: {
       text: titleText
@@ -255,64 +236,69 @@ Template.site.onRendered(function() {
 
           // store yAxis options in separate variable
           const yAxisOptions = seriesData.yAxis;
-
+					yAxisOptions.startOnTick= false;
+            yAxisOptions.endOnTick = false;
           delete seriesData.yAxis;
 
           // insert object into Charts if not yet exists and create new chart
+					const cretaID = `${subType}${metric}`;
+					console.log(`added ${cretaID}`);
           if (!Charts.findOne({
-            _id: subType
+            _id: cretaID
           }, {reactive: false})) {
-            Charts.insert({
-              _id: subType,
-              yAxis: [{
-                  metric
-                }]
+
+             Charts.insert({
+              _id: cretaID,
+              // yAxis: [{
+              //     metric
+              //   }]
             });
 
             const seriesOptions = [];
             seriesOptions.push(seriesData);
-            yAxisOptions.id = metric;
-            createChart(`container-chart-${subType}`, subType, seriesOptions, yAxisOptions);
+          //  yAxisOptions.id = metric;
+            createChart(`container-chart-${cretaID}`, cretaID, seriesOptions, yAxisOptions);
+
           } else {
-            // put axis for each series
-            const chart = $(`#container-chart-${subType}`).highcharts();
-
-            // Add another axis if not yet existent
-            let axisExist = false;
-
-            Charts.findOne({_id: subType}).yAxis.forEach(function(axis) {
-              if (axis.metric === metric) {
-                axisExist = true;
-              }
-            });
-
-            if (!axisExist) {
-              yAxisOptions.opposite = true;
-              yAxisOptions.id = metric;
-              chart.addAxis(yAxisOptions);
-              Charts.update(subType, {
-                $push: {
-                  yAxis: {
-                    metric
-                  }
-                }
-              });
-            }
-
-            // Now just find the right axis index and assign it to the seriesData
-            let axisIndex = 0;
-            Charts.findOne({_id: subType}).yAxis.forEach(function(axis, i) {
-              if (axis.metric === metric) {
-                if (i === 0) { // navigator axis will be at index 1
-                  axisIndex = 0;
-                } else {
-                  axisIndex = i + 1;
-                }
-              }
-            });
-            seriesData.yAxis = axisIndex;
-            chart.addSeries(seriesData);
-          }
+          //   // put axis for each series
+             const chart = $(`#container-chart-${cretaID}`).highcharts();
+					//
+          //   // Add another axis if not yet existent
+          //   let axisExist = false;
+					//
+          //   Charts.findOne({_id: subType}).yAxis.forEach(function(axis) {
+          //     if (axis.metric === metric) {
+          //       axisExist = true;
+          //     }
+          //   });
+					//
+          //   if (!axisExist) {
+          //     yAxisOptions.opposite = true;
+          //     yAxisOptions.id = metric;
+          //     chart.addAxis(yAxisOptions);
+          //     Charts.update(subType, {
+          //       $push: {
+          //         yAxis: {
+          //           metric
+          //         }
+          //       }
+          //     });
+          //   }
+					//
+          //   // Now just find the right axis index and assign it to the seriesData
+          //   let axisIndex = 0;
+          //   Charts.findOne({_id: subType}).yAxis.forEach(function(axis, i) {
+          //     if (axis.metric === metric) {
+          //       if (i === 0) { // navigator axis will be at index 1
+          //         axisIndex = 0;
+          //       } else {
+          //         axisIndex = i + 1;
+          //       }
+          //     }
+          //   });
+          //   seriesData.yAxis = axisIndex;
+             chart.addSeries(seriesData);
+           }
         }
       }
     });
@@ -441,3 +427,126 @@ Template.site.events({
     DataExporter.getDataTCEQ(Router.current().params._id, startEpoch.get(), moment.unix(startEpoch.get()).add(4320, 'minutes').unix());
   }
 });
+
+/**
+ * Highcharts plugin for manually scaling Y-Axis range.
+ *
+ * Author: Roland Banguiran
+ * Email: banguiran@gmail.com
+ *
+ * Usage: Set scalable:false in the yAxis options to disable.
+ * Default: true
+ */
+
+// JSLint options:
+/*global Highcharts, document */
+
+(function (H) {
+    'use strict';
+    var addEvent = H.addEvent,
+        each = H.each,
+        doc = document,
+        body = doc.body;
+
+    H.wrap(H.Chart.prototype, 'init', function (proceed) {
+
+        // Run the original proceed method
+        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+
+        var chart = this,
+            renderer = chart.renderer,
+            yAxis = chart.yAxis;
+
+        each(yAxis, function (yAxis) {
+            var options = yAxis.options,
+                scalable = options.scalable === undefined ? true : options.scalable,
+                labels = options.labels,
+                pointer = chart.pointer,
+                labelGroupBBox,
+                bBoxX,
+                bBoxY,
+                bBoxWidth,
+                bBoxHeight,
+                isDragging = false,
+                downYValue;
+
+            if (scalable) {
+                bBoxWidth = 40;
+                bBoxHeight = chart.containerHeight - yAxis.top - yAxis.bottom;
+                bBoxX = yAxis.opposite ? (labels.align === 'left' ? chart.containerWidth - yAxis.right : chart.containerWidth - (yAxis.right + bBoxWidth)) : (labels.align === 'left' ? yAxis.left : yAxis.left - bBoxWidth);
+                bBoxY = yAxis.top;
+
+                // Render an invisible bounding box around the y-axis label group
+                // This is where we add mousedown event to start dragging
+                labelGroupBBox = renderer.rect(bBoxX, bBoxY, bBoxWidth, bBoxHeight)
+                    .attr({
+                        fill: '#fff',
+                        opacity: 0,
+                        zIndex: 8
+                    })
+                    .css({
+                        cursor: 'ns-resize'
+                    })
+                    .add();
+
+                labels.style.cursor = 'ns-resize';
+
+                addEvent(labelGroupBBox.element, 'mousedown', function (e) {
+                    var downYPixels = pointer.normalize(e).chartY;
+
+                    downYValue = yAxis.toValue(downYPixels);
+                    isDragging = true;
+                });
+
+                addEvent(chart.container, 'mousemove', function (e) {
+                    if (isDragging) {
+                        body.style.cursor = 'ns-resize';
+
+                        var dragYPixels = chart.pointer.normalize(e).chartY,
+                            dragYValue = yAxis.toValue(dragYPixels),
+
+                            extremes = yAxis.getExtremes(),
+                            userMin = extremes.userMin,
+                            userMax = extremes.userMax,
+                            dataMin = extremes.dataMin,
+                            dataMax = extremes.dataMax,
+
+                            min = userMin !== undefined ? userMin : dataMin,
+                            max = userMax !== undefined ? userMax : dataMax,
+
+                            newMin,
+                            newMax;
+
+                        // update max extreme only if dragged from upper portion
+                        // update min extreme only if dragged from lower portion
+                        if (downYValue > (dataMin + dataMax) / 2) {
+                            newMin = min;
+                            newMax = max - (dragYValue - downYValue);
+                            newMax = newMax > dataMax ? newMax : dataMax; //limit
+                        } else {
+                            newMin = min - (dragYValue - downYValue);
+                            newMin = newMin < dataMin ? newMin : dataMin; //limit
+                            newMax = max;
+                        }
+
+                        yAxis.setExtremes(newMin, newMax, true, false);
+                    }
+                });
+
+                addEvent(document, 'mouseup', function () {
+                    body.style.cursor = 'default';
+                    isDragging = false;
+                });
+
+                // double-click to go back to default range
+                addEvent(labelGroupBBox.element, 'dblclick', function () {
+                    var extremes = yAxis.getExtremes(),
+                        dataMin = extremes.dataMin,
+                        dataMax = extremes.dataMax;
+
+                    yAxis.setExtremes(dataMin, dataMax, true, false);
+                });
+            }
+        });
+    });
+}(Highcharts));
