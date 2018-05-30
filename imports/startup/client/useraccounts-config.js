9720f2c3fb741701755abb59893bad62dfb322b9
Peggy Lindner <peggylind@gmail.com>
2018-05-30 18:45:05 -0500

    - removed sign up function from accounts - fixes #19

--- imports/startup/client/useraccounts-config.js
+++ imports/startup/client/useraccounts-config.js
@@ -1,31 +1,8 @@
+import { Accounts } from 'meteor/accounts-base';
+
 Accounts.config(
   {
     sendVerificationEmail: true,
-    forbidClientAccountCreation: false
+    forbidClientAccountCreation: true
   }
 );
-
-Accounts.ui.config({
-  passwordSignupFields: 'EMAIL_ONLY',
-  extraSignupFields: [
-    {
-      fieldName: 'first-name',
-      fieldLabel: 'First name',
-      inputType: 'text',
-      visible: true,
-      validate: function (value, errorFunction) {
-        if (!value) {
-          errorFunction('Please write your first name');
-          return false;
-        } else {
-          return true;
-        }
-      }
-    }, {
-      fieldName: 'last-name',
-      fieldLabel: 'Last name',
-      inputType: 'text',
-      visible: true
-    }
-  ]
-});
