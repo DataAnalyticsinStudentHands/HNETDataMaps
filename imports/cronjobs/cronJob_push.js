63cfe26947862b3b0b8a191af84ebbccb24de88a
Peggy Lindner <peggylind@gmail.com>
2019-10-14 21:23:51 -0500

    - added using new feature TCEQPushing for sites, to check whether the site has been turned on in general to Push to TCEQ. - this is the server part in the code used by sites - fixes #23

--- imports/cronjobs/cronJob_push.js
+++ imports/cronjobs/cronJob_push.js
@@ -2,12 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { pushSiteData } from '../methods/pushSiteDataFunctions';
 import { LiveSites } from '../api/collections_server';
 
-// push data if site is active
+// push data if site and TCEQPushing are Active
 Meteor.setInterval(() => {
   // get site reading aqsid from environment (*.json pm2 config file)
   const site = LiveSites.findOne({ AQSID: process.env.aqsid });
 
-  if (site.status === 'Active') {
+  if (site.status === 'Active' && site.TCEQPushing === 'Active') {
     pushSiteData(process.env.aqsid, false);
   }
 }, 10 * 60 * 1000); // run every 10 min
--- imports/methods/pushSiteDataFunctions.js
+++ imports/methods/pushSiteDataFunctions.js
@@ -182,74 +182,79 @@ export const pushSiteData = function pushSiteData(aqsid) {
   try {
     // get site
     const site = LiveSites.findOne({ AQSID: `${aqsid}` });
-    // check last push not older than 1 day (24 hours)
-    if (site.lastPushEpoch > moment().subtract(1, 'days').unix()) {
-      // use last endEpoch as starting point
-      const lastPush = Exports.findOne({ pushEpoch: site.lastPushEpoch });
-      let startEpoch;
-      if (lastPush === undefined) {
-        // or if just has been resetted
-        startEpoch = site.lastPushEpoch;
-      } else {
-        startEpoch = lastPush.endEpoch;
-        if (startEpoch === undefined) {
+    // check whether this site should push at all
+    if (site.TCEQPushing === 'Active') {
+      // check last push not older than 1 day (24 hours)
+      if (site.lastPushEpoch > moment().subtract(1, 'days').unix()) {
+        // use last endEpoch as starting point
+        const lastPush = Exports.findOne({ pushEpoch: site.lastPushEpoch });
+        let startEpoch;
+        if (lastPush === undefined) {
           // or if just has been resetted
           startEpoch = site.lastPushEpoch;
-        }
-      }
-
-      // try to find everything that is available until now
-      const endEpoch = moment().unix();
-
-      logger.info(`Trying to get data for ${startEpoch} - ${endEpoch}`);
-
-      const data = exportDataAsCSV(aqsid, startEpoch, endEpoch);
-
-      if (Object.keys(data).length === 0 && data.constructor === Object) {
-        throw new Error(`Could not find data for ${startEpoch} - ${endEpoch}`);
-      }
-
-      const startTimeStamp = `${data.data[0].dateGMT} ${data.data[0].timeGMT}`;
-      const endTimeStamp = `${_.last(data.data).dateGMT} ${_.last(data.data).timeGMT}`;
-
-      const outputFile = createTCEQData(aqsid, data);
-
-      const ftps = new FTPS({ host: 'ftps.tceq.texas.gov', username: 'jhflynn@central.uh.edu', password: hnetsftp, protocol: 'ftps', port: 990 });
-
-      // Set up a future
-      const fut = new Future();
-      // call ftps async method
-      ftps.cd('UH/c696').addFile(outputFile).exec((err, res) => {
-        if (res.error) {
-          fut.throw(`Error during push file: ${res.error}`);
         } else {
-          logger.info(`Automatic Push of ${outputFile} ${JSON.stringify(res)}`);
-          // Return the results
-          fut.return(`${outputFile}`);
+          startEpoch = lastPush.endEpoch;
+          if (startEpoch === undefined) {
+            // or if just has been resetted
+            startEpoch = site.lastPushEpoch;
+          }
         }
-      });
 
-      const result = fut.wait();
-      // insert a timestamp for the pushed data
-      const newLastPushEpoch = moment().unix();
-      Exports.insert({
-        _id: `${aqsid}_${moment().unix()}`,
-        pushEpoch: newLastPushEpoch,
-        site: aqsid,
-        startEpoch: moment.utc(startTimeStamp, 'YY/MM/DD HH:mm:ss').unix(),
-        endEpoch: moment.utc(endTimeStamp, 'YY/MM/DD HH:mm:ss').unix(),
-        fileName: result,
-        manual: false
-      });
+        // try to find everything that is available until now
+        const endEpoch = moment().unix();
 
-      // update last push epoch
-      LiveSites.update({
-        AQSID: aqsid
-      }, {
-        $set: {
-          lastPushEpoch: newLastPushEpoch
+        logger.info(`Trying to get data for ${startEpoch} - ${endEpoch}`);
+
+        const data = exportDataAsCSV(aqsid, startEpoch, endEpoch);
+
+        if (Object.keys(data).length === 0 && data.constructor === Object) {
+          throw new Error(`Could not find data for ${startEpoch} - ${endEpoch}`);
         }
-      });
+
+        const startTimeStamp = `${data.data[0].dateGMT} ${data.data[0].timeGMT}`;
+        const endTimeStamp = `${_.last(data.data).dateGMT} ${_.last(data.data).timeGMT}`;
+
+        const outputFile = createTCEQData(aqsid, data);
+
+        const ftps = new FTPS({ host: 'ftps.tceq.texas.gov', username: 'jhflynn@central.uh.edu', password: hnetsftp, protocol: 'ftps', port: 990 });
+
+        // Set up a future
+        const fut = new Future();
+        // call ftps async method
+        ftps.cd('UH/c696').addFile(outputFile).exec((err, res) => {
+          if (res.error) {
+            fut.throw(`Error during push file: ${res.error}`);
+          } else {
+            logger.info(`Automatic Push of ${outputFile} ${JSON.stringify(res)}`);
+            // Return the results
+            fut.return(`${outputFile}`);
+          }
+        });
+
+        const result = fut.wait();
+        // insert a timestamp for the pushed data
+        const newLastPushEpoch = moment().unix();
+        Exports.insert({
+          _id: `${aqsid}_${moment().unix()}`,
+          pushEpoch: newLastPushEpoch,
+          site: aqsid,
+          startEpoch: moment.utc(startTimeStamp, 'YY/MM/DD HH:mm:ss').unix(),
+          endEpoch: moment.utc(endTimeStamp, 'YY/MM/DD HH:mm:ss').unix(),
+          fileName: result,
+          manual: false
+        });
+
+        // update last push epoch
+        LiveSites.update({
+          AQSID: aqsid
+        }, {
+          $set: {
+            lastPushEpoch: newLastPushEpoch
+          }
+        });
+      }
+    } else {
+      logger.info(`Site ${site} is turned off for automatic pushing.`);
     }
   } catch (err) {
     logger.error(err.message);
