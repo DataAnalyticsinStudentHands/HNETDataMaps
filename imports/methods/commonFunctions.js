db574d64881c4f41dbd8dd750c4ceb5a59ebbc42
Shane <dogunbound5@gmail.com>
2020-10-26 12:52:32 -0500

    perform5minagg: Made comments more clear. I guess I should have put it into the previous commit
    Signed-off-by: Shane <dogunbound5@gmail.com>

--- imports/methods/commonFunctions.js
+++ imports/methods/commonFunctions.js
@@ -318,6 +318,7 @@ function perform5minAggregat(siteId, startEpoch, endEpoch) {
             if (parseInt(subTypeName) % 2 === 0) {
               // Even - Needs flag from TAP01
               // Make sure that tap data has a corresponding timestamp in DaqFactory file
+							// If not, break and do not aggregate datapoint
               epochDiff = subObj.epoch - TAP01Epoch;
               if (epochDiff >= 0 && epochDiff < 10) {
                 data[0].val = TAP01Flag;
@@ -327,6 +328,7 @@ function perform5minAggregat(siteId, startEpoch, endEpoch) {
             } else {
               // Odd - Needs flag from TAP02
               // Make sure that tap data has a corresponding timestamp in DaqFactory file
+							// If not, break and do not aggregate datapoint
               epochDiff = subObj.epoch - TAP02Epoch;
               if (epochDiff >= 0 && epochDiff < 10) {
                 data[0].val = TAP02Flag;
