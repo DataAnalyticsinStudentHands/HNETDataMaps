dad7b098d1ab8e5a8e0fe1ff2f0793da21c0f27a
Peggy Lindner <plindner@uh.edu>
2021-04-30 20:57:29 -0500

    removed unnecessary variable globalsite from server startup in front-end

--- imports/startup/server/startup.js
+++ imports/startup/server/startup.js
@@ -1,8 +1,8 @@
 // global variables to be read from environment
-import { LiveSites } from '../../api/collections_server';
+//import { LiveSites } from '../../api/collections_server';
 
 // Setting up directory in which this server expects incoming files (uses an environment variable)
-export const globalsite = LiveSites.findOne({ AQSID: `${process.env.aqsid}` });
+//export const globalsite = LiveSites.findOne({ AQSID: `${process.env.aqsid}` });
 
 // reading ftps password from environment
 export const hnetsftp = process.env.hnetsftp;
