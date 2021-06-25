62d933725941362aed42fcdf6bc165edf7ca4102
dogunbound <dogunbound5@gmail.com>
2021-06-25 12:50:44 -0500

    Stability update. Restart program if it somehow manages to get stuck

--- imports/startup/startup-logger.js
+++ imports/startup/startup-logger.js
@@ -1,8 +1,13 @@
 // Just allows us to use pretty colors when output stuff to console :)
 const util = require('util');
 const logger = {
+	lastDebugDate: null, 
+	lastInfoDate: null, 
+	lastWarnDate: null, 
+	lastErrorDate: null,
 	// fullObject just allows you to ask whether you want the entire object displayed or not
 	debug: function(log, fullObject = true) {
+		lastDebugDate = new Date();
 		if (fullObject) {
 			console.log('\x1b[34m%s\x1b[0m', `debug: ${util.inspect(log, showHidden=true, depth=1000)}`);
 		} else {
@@ -10,6 +15,7 @@ const logger = {
 		}
 	},
 	info: function(log, fullObject = true) {
+		lastInfoDate = new Date();
 		if (fullObject) {
 			console.log('\x1b[32m%s\x1b[0m', `info: ${util.inspect(log, showHidden=true, depth=1000)}`);
 		} else {
@@ -17,6 +23,7 @@ const logger = {
 		}
 	},
 	warn: function(log, fullObject = true) {
+		lastWarnDate = new Date();
 		if (fullObject) {
 			console.log('\x1b[33m%s\x1b[0m', `warning: ${util.inspect(log, showHidden=true, depth=1000)}`);
 		} else {
@@ -24,6 +31,7 @@ const logger = {
 		}
 	},
 	error: function(log, fullObject = true) {
+		lastErrorDate = new Date();
 		if (fullObject) {
 			console.log('\x1b[31m\x1b[1m%s\x1b[0m', `error: ${util.inspect(log, showHidden=true, depth=1000)}`);
 		} else {
--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-/* Old meteor startup code
-import '../imports/startup/startup-logger';
-import '../imports/startup/server/startup';
-import '../imports/methods/oldfeedFunctions';
-*/
-
-// import { checkInitialEnvironmentVariables } from './imports/startup/server/startup.js';
-// import { logger } from './imports/startup/startup-logger.js';
-// import { interval } from './imports/methods/oldfeedFunctions.js';
 const checkInitialEnvironmentVariables = require('./imports/startup/server/startup').checkInitialEnvironmentVariables;
 const logger = require('./imports/startup/startup-logger').logger;
 const interval = require('./imports/methods/oldfeedFunctions.js').interval;
+
+// Works for Date format. Not epoch unless you change it to milliseconds.
+function getDifferenceInDays(date1, date2) {
+  const diffInMs = Math.abs(date2 - date1);
+  return diffInMs / (1000 * 60 * 60 * 24);
+}
+
+// This is to ensure it never gets stuck. 
+// Occasionally, it does and I don't feel like debugging it.
+// This is due to it's random nature of getting stuck.
+// Forcing it to restart whenever a logger.info, error, or warn hasn't printed to console in 3 days is much easier.
+setInterval(() => {
+	let currDate = new Date();
+	if (
+		(logger.lastInfoDate === null || getDifferenceInDays(currDate, logger.lastInfoDate) > 0) &&
+		(logger.lastWarnDate === null || getDifferenceInDays(currDate, logger.lastWarnDate) > 0) &&
+		(logger.lastErrorDate === null || getDifferenceInDays(currDate, logger.lastErrorDate) > 0)
+	) {
+		throw new Error("Nothing has been logged in 3 days. Potentially stuck. Forcing restart.");
+	}
+}, 100000);
 
 checkInitialEnvironmentVariables();
 interval();
